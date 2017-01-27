fc67a0d519596b8289c53dbed796096e4634c0a2
Jamie Corr <jamiecorr4@gmail.com>
2017-01-27 10:59:26 -0800

    Reverting travis test

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
-defsd index():
-    return send_file("templates/index.adgdafghtml")
+def index():
+    return send_file("templates/index.html")
 	
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-.length
