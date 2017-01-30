4a8393d3c77bdad43dbdd96fa206423fccf552f4
Alexandra Deany <alexdeany@pcp078245pcs.wireless.calpoly.edu>
2017-01-30 10:50:33 -0800

    committing changes to chaircontroller

--- static/chair/js/chair.js
+++ static/chair/js/chair.js
@@ -2,5 +2,10 @@ angular.module('TheApples').controller("chairHome", function($scope, $rootScope,
    
    $scope.name = "Timothy Kearns";
    $scope.numNotifications = 12;
+
+   $scope.goToPreferences = function() {
+      $rootScope.role = "chair";
+      $location.path("/facultyPreferences");
+   }
    
 })
