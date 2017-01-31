322f3807129f8306761f28a77a594f15de034b03
jamiecorr <jamiecorr4@gmail.com>
2017-01-31 14:44:35 -0800

    CS2-82 Back button styling

--- static/student/js/student.js
+++ static/student/js/student.js
@@ -1,9 +1,14 @@
 angular.module('TheApples')
 
 .controller("viewScheduleCalendarStudent", function($scope, $rootScope, $location) {
-   
+   $scope.backButtonClicked = function() {
+      $location.path("/login");
+   }
 })
 
 .controller("viewScheduleTableStudent", function($scope, $rootScope, $location) {
-   
+   $scope.backButtonClicked = function() {
+      $location.path("/login");
+   }
 })
+
