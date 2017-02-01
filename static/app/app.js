9e7841eb8c1295a420fafc2c62f236622871ff11
jamiecorr <jamiecorr4@gmail.com>
2017-02-01 12:04:32 -0800

    Import data page started and feedback popup textfield auto layout fix

--- static/app/app.js
+++ static/app/app.js
@@ -112,7 +112,7 @@ app.config(['$routeProvider', function($routeProvider) {
     .when('/importData', {
        templateUrl: 'static/chair/html/importData.html',
        controller: "importData",
-       css: "static/chair/css/importData.css"
+       css: "static/chair/css/chair.css"
     })
     .when('/notifications', {
        templateUrl: 'static/chair/html/notifications.html',
