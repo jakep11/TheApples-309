d8db471e5723f5055e4b60e1a79e6fb9f742b032
zrmartin <zrmartin@calpoly.edu>
2017-01-31 20:43:31 -0800

    Adding initial DB model

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, send_file
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://db:password@35.160.55.128/cs2scheduling'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -10,3 +14,4 @@ def index():
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
+from app import db
