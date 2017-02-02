c98ba805284801a7b4d2fc54a1945ca826bcb8df
Jake Pickett <j.pickett67@yahoo.com>
2017-02-02 09:02:03 -0800

    Correctly linked python files so that API and DB work

--- database/login.py
+++ database/login.py
@@ -1,15 +1,14 @@
 from flask import Blueprint, render_template, flash, redirect, request, url_for, jsonify
 
-#It breaks when you uncomment the line below
-#Zach pleaseeee figure this out. 
-#from models import *
-
-
 login_api = Blueprint('login_api', __name__)
 
+from models import *
+from web_app import db
 
-@login_api.route('/validateLogin', methods = ["GET"])
-def validate_login():
+@login_api.route('/testApiEndpoint', methods = ["GET"])
+def sample_function():
+    #user = User.query.filter_by(username=username).first_or_404()
+    #return user
     return jsonify(id=1, role='chair', username='jake')
 
     # tree_id = request.args.get('id', -1, type = int)
@@ -22,16 +21,47 @@ def validate_login():
     #     else:
     #         return 'ERROR RETRIEVING TREE!'
 
+@login_api.route('/validateLogin', methods = ["POST"])
+def validate_login2():
+    uname = request.args.get('username') 
+    print 'yo'
+    print uname
+    print 'hi'
+    print 'hi'
+    user = User.query.filter_by(username=uname).first()
+    isUser = True
+    if user is None:
+        isUser = False
+    return jsonify(isUser=isUser)
+    #return jsonify(id=1, role='chair', username='jake')
 
-# @app.route('/newUser', methods = ["POST"])
-# def retrieve_fact():
+    # tree_id = request.args.get('id', -1, type = int)
+    # if tree_id >= 0: #non-invalid id in request
+    #     tree = models.Tree.query.filter_by(id=tree_id).first()
+    #     if tree is not None: #results produced
+    #         return jsonify(id=tree.id,
+    #                         topic=tree.topic,
+    #                         user=tree.user.username)
+    #     else:
+    #         return 'ERROR RETRIEVING TREE!'
+
+
+@login_api.route('/newUser', methods = ["POST"])
+def retrieve_fact():
     
-#     fact_id = request.args.get('id', -1, type = int)
-#     if fact_id >= 0: #non-invalid id in request
-#         fact = models.Fact.query.filter_by(id=fact_id).first()
-#         if fact is not None: #results produced
-#             return jsonify(id=fact.id,
-#                             tags=fact.tags,
-#                             quote=fact.quote)
-#         else:
-#             return 'ERROR RETRIEVING FACT!'
+    username = request.args.get('username')
+    password = request.args.get('password')
+    print username, password
+    newUser = User(username=username, password=password)
+    db.session.add(newUser)
+    db.session.commit()
+    return newUser
+    # fact_id = request.args.get('id', -1, type = int)
+    # if fact_id >= 0: #non-invalid id in request
+    #     fact = models.Fact.query.filter_by(id=fact_id).first()
+    #     if fact is not None: #results produced
+    #         return jsonify(id=fact.id,
+    #                         tags=fact.tags,
+    #                         quote=fact.quote)
+    #     else:
+    #         return 'ERROR RETRIEVING FACT!'
--- database/models.py
+++ database/models.py
@@ -1,9 +1,4 @@
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# import ../db.py
-from app import db
-#import app
-
+from web_app import app, db
 
 #-- Description: Stores information and role type for each user of the system
 class User(db.Model):							
