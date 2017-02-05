1881ac85b924b87087a0b827807e9a1a69d18c94
Ben Whittle <b.whitty94@gmail.com>
2017-02-05 13:53:57 -0800

    Added filters.py and updated app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from web_app import app
 
 import database.login
+import database.filters
 
 app.register_blueprint(database.login.login_api, url_prefix="/users")
+app.register_blueprint(database.filters.filters_api, url_prefix="/view")
 
 @app.route("/")
 def index():
