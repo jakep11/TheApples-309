c0471fceaf4bc340341ca2093ea2660d3246be55
Ben Whittle <b.whitty94@gmail.com>
2017-02-05 14:56:00 -0800

    Updated filters.py

--- database/filters.py
+++ database/filters.py
@@ -8,18 +8,34 @@ from web_app import db
 # grab all courses
 @filters_api.route('/allCourses', methods=["GET"])
 def all_courses():
-    courses = Courses.query.all()
-    return jsonify([i.serialize for i in courses])
+   courses = Courses.query.all()
+   return jsonify([i.serialize for i in courses])
 
 
+# grab filtered courses
 @filters_api.route('/filteredCourses', methods=["GET"])
 def filtered_courses():
-    courses = Courses.query.all()
-    return jsonify([i.serialize for i in courses])
+   courses = Courses.query.all()
+   return jsonify([i.serialize for i in courses])
 
 
 # grab all sections
 @filters_api.route('/allSections', methods=["GET"])
 def all_sections():
-    sections = Sections.query.all()
-    return jsonify([i.serialize for i in sections])
+   sections = Sections.query.all()
+   return jsonify([i.serialize for i in sections])
+
+# grab all sections
+@filters_api.route('/filteredSections', methods=["POST"])
+def filtered_sections():
+
+   data = request.json
+
+   terms = data['terms']
+   courses = data['courses']
+   instructors = data['instructors']
+   startTimes = data['startTimes']
+   endTimes = data['endTimes']
+
+   sections = Sections.query.all()
+   return jsonify([i.serialize for i in sections])
