cc4a046f150e55426403bb24b5f1eb85e1b56d0d
Ben Whittle <b.whitty94@gmail.com>
2017-02-11 17:20:56 -0800

    Changed start/end times to use ng-repeat and set their ng-model

--- static/student/js/student.js
+++ static/student/js/student.js
@@ -1,6 +1,71 @@
 var app = angular.module('TheApples');
 
 app.controller("viewScheduleTableStudent", function ($scope, $rootScope, $location, $http) {
+    // lists of time integer values and their corresponding 12 hour time
+    $scope.startTimes = {
+        700: "7:00AM",
+        730: "7:30AM",
+        800: "8:00AM",
+        830: "8:30AM",
+        900: "9:00AM",
+        930: "9:30AM",
+        1000: "10:00AM",
+        1030: "10:30AM",
+        1100: "11:00AM",
+        1130: "11:30AM",
+        1200: "12:00PM",
+        1230: "12:30PM",
+        1300: "1:00PM",
+        1330: "1:30PM",
+        1400: "2:00PM",
+        1430: "2:30PM",
+        1500: "3:00PM",
+        1530: "3:30PM",
+        1600: "4:00PM",
+        1630: "4:30PM",
+        1700: "5:00PM",
+        1730: "5:30PM",
+        1800: "6:00PM",
+        1830: "6:30PM",
+        1900: "7:00PM",
+        1930: "7:30PM",
+        2000: "8:00PM",
+        2030: "8:30PM",
+        2100: "9:00PM",
+    };
+
+    $scope.endTimes = {
+        800: "8:00AM",
+        830: "8:30AM",
+        900: "9:00AM",
+        930: "9:30AM",
+        1000: "10:00AM",
+        1030: "10:30AM",
+        1100: "11:00AM",
+        1130: "11:30AM",
+        1200: "12:00PM",
+        1230: "12:30PM",
+        1300: "1:00PM",
+        1330: "1:30PM",
+        1400: "2:00PM",
+        1430: "2:30PM",
+        1500: "3:00PM",
+        1530: "3:30PM",
+        1600: "4:00PM",
+        1630: "4:30PM",
+        1700: "5:00PM",
+        1730: "5:30PM",
+        1800: "6:00PM",
+        1830: "6:30PM",
+        1900: "7:00PM",
+        1930: "7:30PM",
+        2000: "8:00PM",
+        2030: "8:30PM",
+        2100: "9:00PM",
+        2130: "9:30PM",
+        2200: "10:00PM"
+    };
+
     // arrays to hold the values for checked checkboxes used for filtering sections
     $scope.checkedCourses = {};
     $scope.checkedInstructors = {};
@@ -42,9 +107,9 @@ app.controller("viewScheduleTableStudent", function ($scope, $rootScope, $locati
 
         // testing selected values arrays
         console.log(ids);
-        console.log(ids);
-        console.log(ids);
-        console.log(ids);
+        console.log(instructors);
+        console.log(startTimes);
+        console.log(endTimes);
 
         // POST filter data to filters.py and retrieve filtered courses
         $http({
